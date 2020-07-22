a8de97043875835964ee186710d5bbb82e0462f3
mishra12 <mishra.mrinal12@gmail.com>
2020-07-22 19:52:57 +0530

    water

--- hungry.py
+++ hungry.py
@@ -1,1 +1,9 @@
-print("i m hungry")
+hungry=input("are you hungry")
+if hungry=="yes":
+    print('eat samosa')
+    print('eat piza')
+    print('eat bargar')
+    else:
+        thirsty=input("are yiu thirsty")
+        if thirsty=="yes"
+        print("drink water")
